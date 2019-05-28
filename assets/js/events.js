001dfcfc70540c9dc2d4cdc8db4b68d356506f55
ashryan <ashleeryan@outlook.com>
2019-05-28 22:08:43 +0100

    Added JS to tagline element.

--- assets/js/events.js
+++ assets/js/events.js
@@ -5,3 +5,22 @@ $(document).ready(function() {
         $(modal).modal()
     })
 })
+
+
+
+let tagLine = document.querySelector('.tagline');
+tagLine.addEventListener('click', function(){
+    this.style.color = 'black';
+});
+
+let content = document.querySelector('.contentHeader');
+content.addEventListener('click', function(){
+    content.style.fontSize = '40px';
+});
+
+let contentText = document.querySelector('.contentText');
+contentText.addEventListener('click', function(){
+    this.style.color = 'gold';
+});
+
+
